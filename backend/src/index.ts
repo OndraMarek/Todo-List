0e7a004b17ef673241cc87025629407dd86efd92
OndraMarek <czondrejmarek@gmail.com>
2024-03-07 14:52:34 +0100

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello World! I'm JSON" });
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});

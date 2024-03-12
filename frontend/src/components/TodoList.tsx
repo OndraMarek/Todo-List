f62f98f1b57f9a5afcce9d53efd23c6d1ce37d5d
OndraMarek <czondrejmarek@gmail.com>
2024-03-12 23:25:44 +0100

    sort todo.done

--- frontend/src/components/TodoList.tsx
+++ frontend/src/components/TodoList.tsx
@@ -8,10 +8,12 @@ interface TodoListProps {
 }
 
 function TodoList({ todos, onDelete, onToggleDone }: TodoListProps) {
+  const sortedTodos = todos.sort((todoA, todoB) => todoA.done === todoB.done ? 0 : todoA.done ? 1 : -1);
+
   return (
     <div className="row justify-content-center align-items-center">
       <ul className="list-group col-lg-8 col-md-12 col-sm-12">
-        {todos.map((todo) => (
+        {sortedTodos.map(todo => (
           <TodoItem key={todo.id} todo={todo} onDelete={onDelete} onToggleDone={onToggleDone} />
         ))}
       </ul>

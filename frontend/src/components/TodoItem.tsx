891871a0a8f3a67c9b2dd303c69a6ff747584300
OndraMarek <czondrejmarek@gmail.com>
2024-04-15 16:27:21 +0200

    Refactor TodoItem component to improve readability and maintainability

--- frontend/src/components/TodoItem.tsx
+++ frontend/src/components/TodoItem.tsx
@@ -7,48 +7,52 @@ type TodoItemProps = {
   onToggleDone: (id: string) => void;
 };
 
+const priorityClassMap = {
+  High: "bg-danger",
+  Medium: "bg-warning",
+  Low: "bg-success",
+};
+const priorityTranslationMap = {
+  High: "Vysoká",
+  Medium: "Střední",
+  Low: "Nízká",
+};
+
 function TodoItem({ todo, onDelete, onToggleDone }: TodoItemProps) {
   const [showAdditionalProperties, setShowAdditionalProperties] =
     useState(false);
-  const priorityClassMap = {
-    High: "bg-danger",
-    Medium: "bg-warning",
-    Low: "bg-success",
-  };
-  const priorityTranslationMap = {
-    High: "Vysoká",
-    Medium: "Střední",
-    Low: "Nízká",
-  };
 
   const handleButtonClick = () => {
-    setShowAdditionalProperties(!showAdditionalProperties);
+    setShowAdditionalProperties((showAdditionalProps) => !showAdditionalProps);
   };
+
+  function getPriorityClass(todo: Todo) {
+    if (todo.done) {
+      return "bg-secondary";
+    }
+    return priorityClassMap[todo.priority as keyof typeof priorityClassMap];
+  }
+
+  function getPriorityTranslation(todo: Todo) {
+    return priorityTranslationMap[
+      todo.priority as keyof typeof priorityTranslationMap
+    ];
+  }
 
   return (
     <li
       key={todo.id}
-      className={` list-group-item list-group-item-action ${
+      className={`list-group-item list-group-item-action ${
         todo.done ? "border-0 list-group-item-dark" : ""
       }`}
     >
       <div className="row">
         <div className="col-md-6 d-flex align-items-center">
-          <p className="mb-0 ">{todo.title}</p>
+          <p className="mb-0">{todo.title}</p>
         </div>
         <div className="col-md-6 text-right">
-          <span
-            className={`align-middle me-5 badge ${
-              todo.done
-                ? "bg-secondary"
-                : priorityClassMap[
-                    todo.priority as keyof typeof priorityClassMap
-                  ] || "bg-secondary"
-            }`}
-          >
-            {priorityTranslationMap[
-              todo.priority as keyof typeof priorityTranslationMap
-            ] || todo.priority}
+          <span className={`align-middle me-5 badge ${getPriorityClass(todo)}`}>
+            {getPriorityTranslation(todo)}
           </span>
           <input
             className="align-middle me-3 checkbox"
@@ -69,7 +73,7 @@ function TodoItem({ todo, onDelete, onToggleDone }: TodoItemProps) {
           </button>
         </div>
       </div>
-      {showAdditionalProperties && !todo.done && (
+      {showAdditionalProperties && !todo.done ? (
         <div className="row">
           <hr className="mt-3" />
           <div className="col-md-6">
@@ -81,7 +85,7 @@ function TodoItem({ todo, onDelete, onToggleDone }: TodoItemProps) {
             <p>{todo.note}</p>
           </div>
         </div>
-      )}
+      ) : null}
     </li>
   );
 }
